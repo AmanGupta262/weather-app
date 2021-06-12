204a72b720a9222f0fc0598b2a3f87f7bb020e32
Aman Gupta <amang5840@gmail.com>
2021-06-12 13:13:48 +0530

    create home page

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import {Typography, Button, Paper, Card, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  dFlex: {
+    display: "flex",
+    alignItems: "center",
+    flexFlow: "column",
+    padding: "1rem",
+  },
+  cardStyles: {
+    backgroundColor: "#181e2394",
+    backdropFilter: "blur(5px)",
+    padding: "1rem",
+    borderRadius: "20px",
+    maxWidth: "400px",
+    textAlign: "center",
+    color: "white",
+    boxShadow: "0 7px 30px -10px rgba(150,170,180,0.5)",
+    margin: "1rem"
+  },
+  margin :{
+      marginBottom: " 1rem"
+  }
+});
 
 function CurrentWeather(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.dFlex}>
+        <Card className={classes.cardStyles}>
+          <CardContent>
+            <Typography variant="h4" className={classes.margin}>
+              Bangalore
+            </Typography>
+            <Typography variant="h1" className={classes.margin}>
+              26 &deg;C
+            </Typography>
+            <Typography variant="subtitle1">Cloudy</Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 }
 
 export default CurrentWeather;
--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -5,8 +5,10 @@ const city = "Bengaluru";
 const lat = "12.9762";
 const lon = "77.6033";
 const exclude = "minutely,hourly,current,alerts";
+const units = "metric";
 
 export const APIUrls = {
-  current: () => `${API_ROOT}weather?q=${city}&appid=${API_KEY}`,
-  sevenDays: () => `${API_ROOT}onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${API_KEY}`,
+  current: () => `${API_ROOT}weather?q=${city}&units=${unit}&appid=${API_KEY}`,
+  sevenDays: () =>
+    `${API_ROOT}onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${API_KEY}`,
 };
