6a6b5a2010bd4f3ec4bc854e4a0563d1d08315aa
Aman Gupta <amang5840@gmail.com>
2021-06-12 11:04:12 +0530

    fix urls

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,10 +1,12 @@
-const API_ROOT = "http://api.openweathermap.org/data/2.5/";
+const API_ROOT = "https://api.openweathermap.org/data/2.5/";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const city = "Bengaluru";
-const days = "7";
+const lat = "12.9762";
+const lon = "77.6033";
+const exclude = "minutely,hourly,current,alerts";
 
 export const APIUrls = {
   current: () => `${API_ROOT}weather?q=${city}&appid=${API_KEY}`,
-  sevenDays: () => `${API_ROOT}forecast/daily?q=${city}&cnt=${days}&appid=${API_KEY}`,
+  sevenDays: () => `${API_ROOT}onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${API_KEY}`,
 };
